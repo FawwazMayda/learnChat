242d6d31ad30e0ac1629dc15ed8d9d83430f015b
fawwazmayda <maydafawwaz@gmail.com>
2020-03-28 16:47:30 +0700

    add Constant

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Muhammad Fawwaz Mayda on 28/03/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
